3f08da75ffa89395a17927b595c974443b410dfb
Nikolay Seskin <nseskin@gmail.com>
2021-03-29 18:56:12 +0300

    filter for search request (#2)

--- app/controllers/search/docs_controller.rb
+++ app/controllers/search/docs_controller.rb
@@ -1,7 +1,14 @@
+#encoding: utf-8
+
 class Search::DocsController < ApplicationController
   include Modules::WithSearch
 
   def index
+    # Protection from chinese seo requests
+    if params[:search].split(//).select {|a| !a.match(/[\wа-яА-Яa-žA-Ž\.\,\:\s\-\—\«\»\+\°\&\%\?\#\@\*\(\)\=]+/) }.any?
+      raise ActionController::RoutingError.new('Not Found')
+    end
+
     search classes: [Doc]
     respond_to do |format|
       format.html
